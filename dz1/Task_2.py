7e32fc3de4f96a423320d42a7ae4460473ba0d0f
GJ <GJ@dog.su>
2023-10-29 11:19:53 +0300

    Ссылка на ДЗ из GitHub

--- dz1/Task_2.py
+++ dz1/Task_2.py
@@ -1,4 +1,3 @@
 print("your second name")
 
-
 # Ссылка из GitHub для отправки преподавателю https://github.com/NZpass/Start_python/pull/1/files
--- dz2/Task_3.py
+++ dz2/Task_3.py
@@ -1,3 +1,6 @@
 print("your age")
 
-# Ссылка GitHub на домашнее заание DZ_2
+
+
+# Ссылка из GitHub на домашнее заание DZ_2 https://github.com/NZpass/Start_python/pull/2/files
+# Ссылку копировать из Pull request вкладка File changed
